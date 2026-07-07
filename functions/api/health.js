10ae27ecf0de6030882a7c13d6912a0745b12bd9
tvprohd <ireentvofficial@gmail.com>
2026-07-07 06:05:46 +0600

    Delete functions/api directory
    1

--- functions/api/health.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest() {
-  return new Response(JSON.stringify({ status: "ok", time: new Date().toISOString() }), {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
-    }
-  });
-}
