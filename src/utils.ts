af42229240b36aedb22308959fe1acfe1a94f9d9
Ireen TV <ireentvofficial@gmail.com>
2026-08-04 20:37:57 +0600

    Update utils.ts
    5446546

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { Channel, CategoryType } from './types';
 
 export const PRIORITY_URL_1 = 'https://raw.githubusercontent.com/srhady/tapmad-bd/refs/heads/main/tapmad_bd.m3u';
-export const PRIORITY_URL_2 = 'https://raw.githubusercontent.com/lotaji/playlist-vip/refs/heads/main/playlist_vip.m3u';
+export const PRIORITY_URL_2 = 'https://raw.githubusercontent.com/sume2024/itv/refs/heads/main/unified_playlist.json';
 export const UNIFIED_URL = '#';
 
 export function normalizeChannelName(name: string): string {
